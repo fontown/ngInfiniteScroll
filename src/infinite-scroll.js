514366f09828f1ed3b3937772186f4a2ada1b9b2
Francisco Aranda <flarandagarcia@gmail.com>
2017-08-03 11:09:43 +0200

    created flaranda-ng-infinite-scroll version

--- src/infinite-scroll.js
+++ src/infinite-scroll.js
@@ -241,9 +241,9 @@ angular.module(MODULE_NAME, [])
       scope.$watch('infiniteScrollContainer', handleInfiniteScrollContainer);
       handleInfiniteScrollContainer(scope.infiniteScrollContainer || []);
 
-      // infinite-scroll-parent establishes this element's parent as the
+      // infinite-scroll- establishes this element's parent as the
       // container infinitely scrolled instead of the whole window.
-      if (attrs.infiniteScrollParent == true) {
+      if (attrs.infiniteScrollParent === true || attrs.infiniteScrollParent === 'true') {
         changeContainer(angular.element(elem.parent()));
       }
 
